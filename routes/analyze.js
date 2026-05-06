5bccb02a93351ad717a34196ab903c17dd6acafb
Abhirag Verma <abhiragverma1@gmail.com>
2026-05-07 01:50:52 +0530

    Bug fixing

--- routes/analyze.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { analyzeSkills } = require("../controllers/analyzeController");
-
-router.post("/analyze", analyzeSkills);
-
-module.exports = router;
